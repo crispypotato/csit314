f632df43be9b886e762b26d8a74c28b25b968922
moond3w <chaiguoliang@gmail.com>
2023-10-19 02:37:03 +0800

    Cleaned up format for GUI

--- createAccBoundary.java
+++ createAccBoundary.java
@@ -11,61 +11,53 @@ public class createAccBoundary extends JFrame implements ActionListener {
     private JButton submitButton;
     private JComboBox<String> roleField, positionField;
 
+    private static final Insets WEST_INSETS = new Insets(5, 0, 5, 5);
+    private static final Insets EAST_INSETS = new Insets(5, 5, 5, 0);
+
     public createAccBoundary() {
 
         FlatDarkLaf.setup();
 
         JPanel panel = new JPanel(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-
-        // Set Header
-        JLabel headerLabel = new JLabel("Create New Account");
-        c.gridx = 0;
-        c.gridy = 0;
-        c.gridwidth = 3;
-        panel.add(headerLabel, c);
+        panel.setBorder(BorderFactory.createCompoundBorder(
+            BorderFactory.createTitledBorder("Create New Account"),
+            BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+        
+        GridBagConstraints c;
         
         // Set name Label
         JLabel nameLabel = new JLabel("Name: ");
-        c.gridx = 0;
-        c.gridy = 1;
-        c.gridwidth = 1;
+        c = createGbc(0,1);
         panel.add(nameLabel, c);
 
         // Set name
         nameField = new JTextField();
-        c.gridx = 1;
-        c.gridy = 1;
+        c = createGbc(1,1);
         panel.add(nameField, c);
 
         // Set Salary label
         JLabel salaryLabel = new JLabel("Salary: ");
-        c.gridx = 0;
-        c.gridy = 2;
+        c = createGbc(0,2);
         panel.add(salaryLabel, c);
 
         // Set salary
         salaryField = new JTextField();
-        c.gridx = 1;
-        c.gridy = 2;
+        c = createGbc(1,2);
         panel.add(salaryField, c);
 
         // Set Date Joined label
         JLabel dateJoinedLabel = new JLabel("Date Joined: ");
-        c.gridx = 0;
-        c.gridy = 3;
+        c = createGbc(0,3);
         panel.add(dateJoinedLabel, c);
 
         // Set Date Joined
         dateJoinedField = new JTextField();
-        c.gridx = 1;
-        c.gridy = 3;
+        c = createGbc(1,3);
         panel.add(dateJoinedField, c);
 
         // Set Role Label
         JLabel roleLabel = new JLabel("Role: ");
-        c.gridx = 0;
-        c.gridy = 4;
+        c = createGbc(0,4);
         panel.add(roleLabel, c);
 
         // Set Role
@@ -74,14 +66,12 @@ public class createAccBoundary extends JFrame implements ActionListener {
         roleField.setVisible(true);
         roleField.setSelectedIndex(0);
         roleField.addActionListener(this);
-        c.gridx = 1;
-        c.gridy = 4;
+        c = createGbc(1,4);
         panel.add(roleField,c);
 
         // Set Position Label
         JLabel positionLabel = new JLabel("Position: ");
-        c.gridx = 0;
-        c.gridy = 5;
+        c = createGbc(0,5);
         panel.add(positionLabel, c);
         
         // Set Position if Role = Cafe Staff
@@ -89,43 +79,35 @@ public class createAccBoundary extends JFrame implements ActionListener {
         positionField = new JComboBox<String>(positionChoices);
         positionField.setVisible(true);
         positionField.setSelectedIndex(0);
-        c.gridx = 1;
-        c.gridy = 5;
+        c = createGbc(1,5);
         panel.add(positionField, c);
 
         // Set username label
         JLabel usernameLabel = new JLabel("Username: ");
-        c.gridx = 0;
-        c.gridy = 6;
+        c = createGbc(0,6);
         panel.add(usernameLabel, c);
 
         // Set username
         usernameField = new JTextField();
-        c.gridx = 1;
-        c.gridy = 6;
+        c = createGbc(1,6);
         panel.add(usernameField, c);
 
         // Set password label
         JLabel passwordLabel = new JLabel("Password: ");
-        c.gridx = 0;
-        c.gridy = 7;
+        c = createGbc(0,7);
         panel.add(passwordLabel, c);
 
         // Set password
         passwordField = new JPasswordField();
-        c.gridx = 1;
-        c.gridy = 7;
+        c = createGbc(1,7);
         panel.add(passwordField, c);
 
         submitButton = new JButton("Submit");
         submitButton.addActionListener(this);
-        c.gridx = 1;
-        c.gridy = 8;
-        c.anchor = GridBagConstraints.PAGE_END; //bottom of space
-        c.insets = new Insets(10,0,0,0);  //top padding
+        c = createGbc(1,8);
         panel.add(submitButton, c);
 
-        final JFrame frame = new JFrame("FlatDarkLaf test.");
+        final JFrame frame = new JFrame("Create New Account");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(panel);
         frame.pack();
@@ -157,6 +139,23 @@ public class createAccBoundary extends JFrame implements ActionListener {
             usernameField.setText("");
             passwordField.setText("");
         }
+    }
+
+    private GridBagConstraints createGbc(int x, int y) {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = x;
+        gbc.gridy = y;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+  
+        gbc.anchor = (x == 0) ? GridBagConstraints.WEST : GridBagConstraints.EAST;
+        gbc.fill = (x == 0) ? GridBagConstraints.BOTH
+              : GridBagConstraints.HORIZONTAL;
+  
+        gbc.insets = (x == 0) ? WEST_INSETS : EAST_INSETS;
+        gbc.weightx = (x == 0) ? 0.1 : 1.0;
+        gbc.weighty = 1.0;
+        return gbc;
     }
 
     public static void main(String[] args) {        
