36613e291f9475567ed68cf258cb99088c1cef94
moond3w <chaiguoliang@gmail.com>
2023-10-25 00:08:06 +0800

    Removed authenAccount since unused

--- loginController.java
+++ loginController.java
@@ -7,22 +7,19 @@ final class loginController {
     // Prevent instantiation of class object
     private loginController() {}
 
-    public static boolean authenAccount(String username, String password)
-    {
-        // Ensure username and password is alphanumeric
-        if (!(isAlphaNumeric(username)) || !(isAlphaNumeric(password)))
-        {
-            return false;
-        }
-
-        boolean authenAccount = User.authenAccount(username, password);
-        return authenAccount;
-    }
-
     public static User loginUser(String username, String password)
     {
-        User currentUser = User.loginUser(username, password);
-        return currentUser;
+        User currentUser;
+
+        if (!(isAlphaNumeric(username)) || !(isAlphaNumeric(password)))
+        {
+            currentUser = User.loginUser(username, password);
+        }
+        else
+        {
+            currentUser = new User();
+        }
+            return currentUser;
     }
 
     // Check if string is strictly alpha-numeric
