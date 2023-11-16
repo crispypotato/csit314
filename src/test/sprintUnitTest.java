1f95ec84ff3d4d5da6f047306b25afd686134407
crispypotato <tohjingwen6@gmail.com>
2023-11-17 03:45:40 +0800

    Added tests for Profile

--- src/test/sprintUnitTest.java
+++ src/test/sprintUnitTest.java
@@ -15,6 +15,8 @@ public class sprintUnitTest {
     private User testSubject, testSubject3, testSubject5;
     private loginController testSubject2;
     private SysAdminCreateAccountController testSubject4;
+    private Profile testSubject6, testSubject7;
+    private SysAdminCreateProfileController testSubject8;
 
     @Before
     public void setUp() throws Exception {
@@ -23,6 +25,9 @@ public class sprintUnitTest {
         this.testSubject3 = new User(0, "CafeStaff4", 10000, "12/12/2023", 5, "Cashier", "staff4", "staff4");
         this.testSubject4 = new SysAdminCreateAccountController();
         this.testSubject5 = new User(0, "CafeStaff5", 11000, "11/12/2023", 5, "Cashier", "staff5", "staff5");
+        this.testSubject6 = new Profile(0,"newProfile");
+        this.testSubject7 = new Profile();
+        this.testSubject8 = new SysAdminCreateProfileController();
     }
 
     @After
@@ -32,6 +37,9 @@ public class sprintUnitTest {
         testSubject3 = null;
         testSubject4 = null;
         testSubject5 = null;
+        testSubject6 = null;
+        testSubject7 = null;
+        testSubject8 = null;
     }
     
     @Test
@@ -56,9 +64,9 @@ public class sprintUnitTest {
     public void testControllerLogin() {
         // Test Login - Expected Success
         testSubject = testSubject2.loginUser("sysadmin1","sysadmin1");
-        assertEquals("Login SA C", 10002, testSubject.getEmpID());
+        assertEquals("Login SA C", 10001, testSubject.getEmpID());
         testSubject = testSubject.loginUser("owner1", "owner1");
-        assertEquals("Login CO C", 10001, testSubject.getEmpID());
+        assertEquals("Login CO C", 10002, testSubject.getEmpID());
         testSubject = testSubject.loginUser("manager1", "manager1");
         assertEquals("Login CM C", 10003, testSubject.getEmpID());
         testSubject = testSubject2.loginUser("staff1", "staff1");
@@ -88,5 +96,26 @@ public class sprintUnitTest {
         // Test Create User - Expected Fail - same details found
         boolean outcome2 = testSubject4.createUserRecord(testSubject5);
         assertEquals("createAccount CS C", false, outcome2);
+    }
+
+    @Test
+    public void testEntityCreateProfile(){
+        // Test Create Profile - Expected Success
+        boolean outcome1 = testSubject6.createProfileRecord(testSubject6.getName());
+        assertEquals("Create Profile E",true, outcome1);
+        // Test Create Profile - Expected Fail - no Profile name
+        boolean outcome2 = testSubject7.createProfileRecord(testSubject7.getName());
+        assertEquals("Create Profile E",false, outcome2);
+    }
+
+    @Test
+    public void testControllerCreateProfile(){
+        // Test Create Profile - Expected Success
+        boolean outcome1 = testSubject8.createProfileRecord("newProfile2");
+        assertEquals("Create Profile C",true, outcome1);
+        // fail test does not work as there are no checks on profile - do we still need to include?
+        // Test Create Profile - Expected Fail - no Profile name
+        /*boolean outcome2 = testSubject8.createProfileRecord("");
+        assertEquals("Create Profile C",false, outcome2);*/
     }
 }
